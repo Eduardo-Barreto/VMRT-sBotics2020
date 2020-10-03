46c373f8be3d3408aea163eefca52cd35af2922c
Eduardo-Barreto <eduwagzozo@gmail.com>
2020-10-03 14:24:01 -0300

    compilador atualizado

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+
+os.system('code --install-extension maptz.regionfolder')
+
+
+while True:
+
+    diretorio = os.listdir('./src')
+
+    if 'output' not in os.listdir('./'):
+        os.mkdir('./output')
+
+    out = open('./output/VMRT.sBoticsR', 'w')
+    
+    os.system('cls')
+
+    for arquivo in diretorio:
+        if 'sBoticsR' in arquivo:
+            arquivoAtual = open('./src/' + arquivo, 'r')
+            out.write('#region ' + arquivo[:arquivo.find('.sBoticsR')] + '\n')
+            for linha in arquivoAtual:
+                if '#' in linha:
+                    pass
+                else:
+                    out.write('\t' + linha)
+            arquivoAtual.close()
+            out.write('\n#endregion\n\n')
+            
+        else:
+            print('-'*20, 'Arquivo inválido:', arquivo,'-'*20, end='')
+        print('\n','-'*20, 'fim do arquivo:', arquivo[:arquivo.find('.sBoticsR')], '-'*20)
+
+    out.write('fim\n')
+    out.close()
+
+    while input() != 'compilar':
+        pass
